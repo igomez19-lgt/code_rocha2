fce989dd9f7a439326e694a4c11d33ae85ab20e0
igomez19-lgt <igomez19@gmail.com>
2026-04-20 10:33:12 -0300

    Update Run_ansys_prisma_Prima_trans_iso_BasaltoIrregular1.m

--- Run_ansys_prisma_Prima_trans_iso_BasaltoIrregular1.m
+++ Run_ansys_prisma_Prima_trans_iso_BasaltoIrregular1.m
@@ -1,66 +1,72 @@
 
 clc; clear;
 
-% Mesh_s_j=[0.009 0.0092 0.0094 0.0095 0.0097 0.0099 0.01];
-Freq=zeros(10-6,1);
+% Preallocate frequency vector (results from ANSYS)
+Freq = zeros(10-6,1);
 
-folder = 'C:\Dados GEO\Artigo Rochas\1.Iteracao2mm'; % Substitua pelo caminho da sua pasta
-
-% Procura todos os arquivos com "file" no nome
+% --- Clean working directory ---
+% Remove previous result files to avoid interference
+folder = 'C:\Dados GEO\Artigo Rochas\1.Iteracao2mm';
 files = dir(fullfile(folder, '*file*'));
 
-% Loop para deletar os arquivos encontrados
 for k = 1:length(files)
-    filePath = fullfile(folder, files(k).name); % Caminho completo do arquivo
-    delete(filePath); % Apaga o arquivo
-%     fprintf('Arquivo apagado: %s\n', filePath);
+    filePath = fullfile(folder, files(k).name);
+    delete(filePath);
 end
 
-a=[7.89017086847299,0.257741269457322];
-% [8.67969519044077,0.216600343571348]
-Poisson=a(1,2);
-Modulo=a(1,1)*1e10;
+% --- Material parameters (calibrated values) ---
+a = [7.89017086847299, 0.257741269457322];
 
-%% Changing the input parameters
-for j=1:length(Poisson)
+Poisson = a(1,2);          % Poisson's ratio
+Modulo  = a(1,1) * 1e10;  % Young's modulus (Pa)
+
+%% --- Update input parameters and run simulation ---
+for j = 1:length(Poisson)
     
-E_X =Modulo; %Var1
-E_Y=Modulo;  %Var2
-E_Z=E_X;   
-PR_XY=Poisson(j);   %Var3
-PR_YZ=PR_XY; %Var4 
-PR_XZ=Poisson(j);   
-G_XY=E_X/(2*(1+PR_XZ));	 %Var5
-G_YZ=G_XY;	
-G_XZ=E_X/(2*(1+PR_XZ));	
- 
-% creating new journal file
-fid  = fopen('Prima_trans_iso_BasaltoIrregular1.txt','r');
-f = fread(fid,'*char')';
-fclose(fid);
-f = strrep(f,'E_X' , num2str(E_X));
-f = strrep(f,'E_Y' , num2str(E_Y));
-f = strrep(f,'E_Z' , num2str(E_Z));
-f = strrep(f,'PR_XY' , num2str(PR_XY));
-f = strrep(f,'PR_YZ' , num2str(PR_YZ));
-f = strrep(f,'PR_XZ' , num2str(PR_XZ));
-f = strrep(f,'G_XY' , num2str(G_XY));
-f = strrep(f,'G_YZ' , num2str(G_YZ));
-f = strrep(f,'G_XZ' , num2str(G_XZ));
+    % Define elastic properties (assumed isotropic behavior)
+    E_X = Modulo;
+    E_Y = Modulo;
+    E_Z = E_X;
 
-fid  = fopen('Prima_trans_iso_Final_BasaltoIrregular1.txt','w');
-fprintf(fid,'%s',f);
-fclose(fid);
+    PR_XY = Poisson(j);
+    PR_YZ = PR_XY;
+    PR_XZ = Poisson(j);
 
-%% Calculations in ansys with input parameters
-% system('C:\"Program Files"\"ANSYS Inc"\v192\Framework\bin\Win64\runwb2.exe -B -R file.db');
-tic
-!"C:\Program Files\ANSYS Inc\v192\ansys\bin\winx64\ANSYS192.exe" -b -i Prima_trans_iso_Final_BasaltoIrregular1.txt -o Output.txt
-toc
-%% saving the output parameter
-load frequencias1.txt
+    % Shear moduli from isotropic relation
+    G_XY = E_X / (2 * (1 + PR_XZ));
+    G_YZ = G_XY;
+    G_XZ = E_X / (2 * (1 + PR_XZ));
 
-Freq(:,j)=frequencias1;
+    % --- Generate updated ANSYS input file ---
+    fid = fopen('Prima_trans_iso_BasaltoIrregular1.txt','r');
+    f = fread(fid,'*char')';
+    fclose(fid);
+
+    % Replace placeholders with current material properties
+    f = strrep(f,'E_X' , num2str(E_X));
+    f = strrep(f,'E_Y' , num2str(E_Y));
+    f = strrep(f,'E_Z' , num2str(E_Z));
+    f = strrep(f,'PR_XY' , num2str(PR_XY));
+    f = strrep(f,'PR_YZ' , num2str(PR_YZ));
+    f = strrep(f,'PR_XZ' , num2str(PR_XZ));
+    f = strrep(f,'G_XY' , num2str(G_XY));
+    f = strrep(f,'G_YZ' , num2str(G_YZ));
+    f = strrep(f,'G_XZ' , num2str(G_XZ));
+
+    fid = fopen('Prima_trans_iso_Final_BasaltoIrregular1.txt','w');
+    fprintf(fid,'%s',f);
+    fclose(fid);
+
+    %% --- Run ANSYS modal analysis ---
+    tic
+    !"C:\Program Files\ANSYS Inc\v192\ansys\bin\winx64\ANSYS192.exe" -b -i Prima_trans_iso_Final_BasaltoIrregular1.txt -o Output.txt
+    toc
+
+    %% --- Import simulation results ---
+    % Load computed natural frequencies
+    load frequencias1.txt
+    Freq(:,j) = frequencias1;
+
 end
 
 
